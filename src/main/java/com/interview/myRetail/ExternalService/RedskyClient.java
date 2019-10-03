3996352980e105d052f32ebc9bfc0677c692ff04
ahunkele <31667935+ahunkele@users.noreply.github.com>
2019-10-03 16:41:21 -0500

    move external service call to own client package

--- src/main/java/com/interview/myRetail/ExternalService/RedskyClient.java
+++ src/main/java/com/interview/myRetail/ExternalService/RedskyClient.java
@@ -1,22 +1,30 @@
 package com.interview.myRetail.ExternalService;
 
+import java.io.IOException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
 /**
  * 
  */
 public class RedskyClient
 {
-//     static final String BASE_URL = "http://redsky.target.com/v2/pdp/tcin/";
-//     static final String EXCLUSION_QUERY = "?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics";
-//     
-//     public static String getProductInfoByID(final String id)
-//     {
-//         RestTemplate restTemplate = new RestTemplate();
-//         final String productInformationURL = BASE_URL + id + EXCLUSION_QUERY;
-//         final String response = restTemplate.getForObject(productInformationURL, String.class);
-//         
-//         System.out.println(response);
-//        
-//         return response;
-//     }
-     
+    static final String BASE_URL = "https://redsky.target.com/v2/pdp/tcin/";
+    static final String EXCLUSION_QUERY = "?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics";
+    
+    public static ResponseEntity<String> getProductInfoByID(final String id) throws JsonParseException, JsonMappingException, IOException
+    {
+        RestTemplate restTemplate = new RestTemplate();
+        String productInformationURL = BASE_URL + id + EXCLUSION_QUERY;
+        ResponseEntity<String> response = restTemplate.getForEntity(productInformationURL, String.class);
+       
+        System.out.println(response.getBody());
+        
+        return response;
+    }
+
 }
--- src/main/java/com/interview/myRetail/Service/ProductService.java
+++ src/main/java/com/interview/myRetail/Service/ProductService.java
@@ -5,11 +5,16 @@
  */
 package com.interview.myRetail.Service;
 
+import java.io.IOException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.interview.myRetail.ExternalService.RedskyClient;
 import com.interview.myRetail.Repositories.ProductRepoitory;
 
 /**
@@ -24,9 +29,9 @@ public final class ProductService
     @Autowired
     private ProductRepoitory productRepoitory;
     
-    public String getProductInfoByID(final String id)
+    public String getProductInfoByID(final String id) throws JsonParseException, JsonMappingException, IOException
     {
-        return "";
+        return RedskyClient.getProductInfoByID(id).getBody();
     }
     
 }
